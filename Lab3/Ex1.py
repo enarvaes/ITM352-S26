a7a2e5538a758375f60b4e8a4cadff02c98fb22e
enarvaes <enarvaes@hawaii.edu>
2026-01-29 22:37:17 -1000

    finished up lab 3

--- Lab3/Ex1.py
+++ Lab3/Ex1.py
@@ -9,3 +9,12 @@ print(f"Encoded text: {encoded_text}")
 #Use the cryptography library to encode and decoode a message
 decoded_text = cipher_suite.decrypt(encoded_text)
 print(f"Decoded text: {decoded_text.decode('utf-8')}")
+
+def max(num1, num2):
+    #Return the maximum of two numbers
+    return num1 if num1 > num2 else num2
+
+def min(num1, num2):
+    #Return the minimum of two numbers
+    return num1 if num1 < num2 else num2
+
